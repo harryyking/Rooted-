c9d11b351217ca92e8516b33d4e4ee98e6c3db1d
harryyking <arthurharry06@gmail.com>
2024-08-18 17:05:43 -0700

    api trouble

--- app/api/getLeaderboard/route.js
+++ app/api/getLeaderboard/route.js
@@ -1,9 +1,19 @@
 import { connectToDB } from "@utils/database"
 import User from "@models/User";
+import { getServerSession } from "next-auth";
 
 export const GET = async(request) => {
     try {
         await connectToDB();
+
+        const session = await getServerSession({req: request});
+
+        if(!session) {
+           return new Response("Unauthorized", {status: 401});
+         }
+
+       const userEmail = session.user.email;
+       console.log(userEmail);
 
         const user = await User.find({}).sort({streak: -1}).limit(80);
 
--- app/api/postRecord/retrieve/route.js
+++ app/api/postRecord/retrieve/route.js
@@ -1,13 +1,21 @@
 import { connectToDB } from "@utils/database";
 import Quiet from "@models/Quiet-Time";
-
+import { getServerSession } from "next-auth";
 
 // API to fetch all quiet time posts 
 export const GET = async (request) => {
     try{
-        await connectToDB()
-       
-        
+        await connectToDB();
+
+        const session = await getServerSession({req: request});
+
+        if(!session) {
+           return new Response("Unauthorized", {status: 401});
+         }
+
+       const userEmail = session.user.email;
+       console.log(userEmail);
+
 
         const quietTime = await Quiet.find({}).populate('creator').sort({lastActivityDate: -1})
         console.log("Fetched all the quiet time posts", quietTime);
